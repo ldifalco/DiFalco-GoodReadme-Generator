09287f8a78d6d385ac3ac30b0036deb8b5a0f9d0
Lucas DiFalco <luke.difalco52@gmail.com>
2021-08-05 10:20:03 -0400

    changed file directory

--- init/index.js
+++ init/index.js
@@ -1,13 +1,18 @@
 //packages needed for application, selecting file for data to go to
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown.js')
+const generateMarkdown = require('./generateMarkdown')
 
 
 // array of questions for inqurier package
 const questions = 
 inquirer
 .prompt([
+    {
+        type: 'input',
+        message: 'name of README',
+        name: 'name'
+    },
 {
     type: 'input',
     message: 'description of README',
@@ -33,17 +38,27 @@ inquirer
     message: 'Test Instructions',
     name: 'instruct'
 },
+{
+    type: 'list',
+    message: 'Select license',
+    choices: ['Apache', 'MIT', 'GPL'],
+    name: 'license'
+
+}
+
 ])
 //console log answers to make sure they register
 .then(function (answer){
+console.log(answer.name);    
 console.log(answer.description);
 console.log(answer.install);
 console.log(answer.useinfo);
 console.log(answer.contribute);
 console.log(answer.instruct);
+console.log(answer.license)
 
 })
-
+//function to write file
 .then((answer) => {
     fs.writeFile('README.md', generateMarkdown(answer), (err) =>
     err ? console.error(err) : console.log('README created!'))
